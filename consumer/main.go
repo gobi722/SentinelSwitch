000a50806d44af37c462f447946f7bd1cb7abf61
Gobi722 <gobinath1410lakshmi@gmail.com>
2026-03-05 09:32:06 +0530

    multi cluster kafka commit

--- consumer/main.go
+++ consumer/main.go
@@ -12,46 +12,64 @@ import (
 func main() {
 
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  []string{"localhost:9092"},
-		GroupID:  "test-group",
-		Topic:    "test-topic",
-		MinBytes: 10e3,
-		MaxBytes: 10e6,
+		Brokers: []string{
+			"localhost:9092",
+			"localhost:9093",
+			"localhost:9094",
+		},
+		GroupID: "payment-group",
+		Topic:   "payments",
 	})
 
-	defer reader.Close()
-
-	fmt.Println("Reliable Consumer started...")
-
-	ctx := context.Background()
-
 	for {
-		// 1 Fetch (NO auto commit)
-		msg, err := reader.FetchMessage(ctx)
+		msg, err := reader.ReadMessage(context.Background())
 		if err != nil {
-			log.Println("Fetch error:", err)
-			continue
+			log.Fatal(err)
 		}
-
-		// 2️ Process
-		err = processMessage(msg)
-		if err != nil {
-			log.Println("Processing failed. Will retry:", err)
-			continue // offset NOT committed
-		}
-
-		// 3️⃣ Commit ONLY if success
-		err = reader.CommitMessages(ctx, msg)
-		if err != nil {
-			log.Println("Commit failed:", err)
-			continue
-		}
-
-		fmt.Printf("Processed & committed: %s | Partition: %d | Offset: %d\n",
-			string(msg.Value),
-			msg.Partition,
-			msg.Offset)
+		log.Printf("Message: %s\n", string(msg.Value))
 	}
+
+	// reader := kafka.NewReader(kafka.ReaderConfig{
+	// 	Brokers:  []string{"localhost:9092"},
+	// 	GroupID:  "test-group",
+	// 	Topic:    "test-topic",
+	// 	MinBytes: 10e3,
+	// 	MaxBytes: 10e6,
+	// })
+
+	// defer reader.Close()
+
+	// fmt.Println("Reliable Consumer started...")
+
+	// ctx := context.Background()
+
+	// for {
+	// 	// 1 Fetch (NO auto commit)
+	// 	msg, err := reader.FetchMessage(ctx)
+	// 	if err != nil {
+	// 		log.Println("Fetch error:", err)
+	// 		continue
+	// 	}
+
+	// 	// 2️ Process
+	// 	err = processMessage(msg)
+	// 	if err != nil {
+	// 		log.Println("Processing failed. Will retry:", err)
+	// 		continue // offset NOT committed
+	// 	}
+
+	// 	// 3️⃣ Commit ONLY if success
+	// 	err = reader.CommitMessages(ctx, msg)
+	// 	if err != nil {
+	// 		log.Println("Commit failed:", err)
+	// 		continue
+	// 	}
+
+	// 	fmt.Printf("Processed & committed: %s | Partition: %d | Offset: %d\n",
+	// 		string(msg.Value),
+	// 		msg.Partition,
+	// 		msg.Offset)
+	// }
 }
 
 func processMessage(msg kafka.Message) error {
@@ -66,6 +84,5 @@ func processMessage(msg kafka.Message) error {
 	// Simulate work
 	time.Sleep(2 * time.Second)
 
-	
 	return nil
 }
--- producer/main.go
+++ producer/main.go
@@ -1,8 +1,8 @@
+
 package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
@@ -12,34 +12,73 @@ import (
 func main() {
 
 	writer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers:  []string{"localhost:9092"},
-		Topic:    "test-topic",
-		Balancer: &kafka.Hash{},
+		Brokers: []string{
+			"localhost:9092",
+			"localhost:9093",
+			"localhost:9094",
+		},
+		Topic:        "payments",
+		// RequiredAcks: kafka.RequireAll, // acks=all
+		Balancer:     &kafka.LeastBytes{},
 	})
-	defer writer.Close()
 
-	for i := 0; i < 20; i++ {
-
-		var value string
-
-		// Inject failure at message-5
-		if i == 5 {
-			value = "fail"
-		} else {
-			value = fmt.Sprintf("message-%d", i)
-		}
-
-		msg := kafka.Message{
-			Key:   []byte(fmt.Sprintf("key-%d", i)),
-			Value: []byte(value),
-		}
-
-		err := writer.WriteMessages(context.Background(), msg)
+	for i := 0; i < 100; i++ {
+		err := writer.WriteMessages(context.Background(),
+			kafka.Message{
+				Key:   []byte("txn"),
+				Value: []byte("payment message"),
+			},
+		)
 		if err != nil {
-			log.Fatal("Write error:", err)
+			log.Fatal(err)
 		}
-
-		fmt.Println("Produced:", value)
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(time.Second)
 	}
 }
+
+
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"time"
+
+// 	"github.com/segmentio/kafka-go"
+// )
+
+// func main() {
+
+// 	writer := kafka.NewWriter(kafka.WriterConfig{
+// 		Brokers:  []string{"localhost:9092"},
+// 		Topic:    "test-topic",
+// 		Balancer: &kafka.Hash{},
+// 	})
+// 	defer writer.Close()
+
+// 	for i := 0; i < 20; i++ {
+
+// 		var value string
+
+// 		// Inject failure at message-5
+// 		if i == 5 {
+// 			value = "fail"
+// 		} else {
+// 			value = fmt.Sprintf("message-%d", i)
+// 		}
+
+// 		msg := kafka.Message{
+// 			Key:   []byte(fmt.Sprintf("key-%d", i)),
+// 			Value: []byte(value),
+// 		}
+
+// 		err := writer.WriteMessages(context.Background(), msg)
+// 		if err != nil {
+// 			log.Fatal("Write error:", err)
+// 		}
+
+// 		fmt.Println("Produced:", value)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
